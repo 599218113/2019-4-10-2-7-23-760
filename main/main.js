59eba1527aed585512240ed636d69cc0291e6a27
zhsq11 <599218113@qq.com>
2019-05-24 12:01:59 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var dis = inputs["distance"];
+    var pT = inputs["parkTime"];
+    var ans = 6;
+    ans += 0.25 * pT;
+    if(dis >= 2) {
+        ans += 0.8 * (dis - 2);
+        if(dis >= 8) {
+            ans += 0.8 * 0.5 * (dis - 8);
+        }
+    }
+    return Math.round(ans);
 };
